05f97c13e4600ee2e8d204245ea68af63bcee407
jashook <jshook@smu.edu>
2014-01-30 21:05:17 -0600

    pushing everything

--- rsch/picture_compare/imaging.py
+++ rsch/picture_compare/imaging.py
@@ -29,74 +29,78 @@ _StoredPictures = dict()
 
 _ImageNames = []
 
+def build_dictionary(_Directory):
+
+   _Files = os.listdir(_Directory)
+
+   for _File in _Files:
+
+      if _File[0] == '.': continue
+
+      _Image = Image.open(_Directory + _File)
+
+      _InImage = image(_File)
+
+      _ImageNames.append(_InImage.m_name)
+
+      _StoredPictures[_InImage.m_name] = _InImage
+
+      _Width, _Height = _Image.size
+
+      _TempWidth = 0
+
+      _TempHeight = 0
+
+      _Offset = 30
+
+      _SecondTime = False
+
+      _SecondTimeWidth = False
+
+      while True:
+
+         _Left = _TempWidth
+
+         _Top = _TempHeight
+
+         _NewSize = [_Left, _Top, _Left + _Offset, _Top + _Offset]
+
+         if (_NewSize[2] > _Width): _NewSize[2] = _Width
+
+         if (_NewSize[3] > _Height): _NewSize[3] = _Height
+
+         _NewPicture = _Image.crop(_NewSize)
+
+         _Hash = hashlib.md5(_NewPicture.tostring()).hexdigest()
+
+         try:
+            
+            _Hashes[_Hash]
+
+            _Hashes[_Hash].append(_InImage)
+
+         except:
+ 
+            _Hashes[_Hash] = list().append(_InImage)
+
+         _InImage.m_pictures.append(_Hash)
+
+         _TempWidth = _TempWidth + _Offset
+
+         if (_TempWidth > _Width):
+
+            _TempWidth = 0
+
+            _TempHeight = _TempHeight + _Offset
+
+         if (_TempHeight >= _Height):
+
+            break
+
 if __name__ == "__main__":
 
    if (len(sys.argv) == 2): 
 
       _Directory = sys.argv[1]
 
-      _Files = os.listdir(_Directory)
-
-      for _File in _Files:
-
-         if _File[0] == '.': continue
-
-         _Image = Image.open(_Directory + _File)
-
-         _InImage = image(_File)
-
-         _ImageNames.append(_InImage.m_name)
-
-         _StoredPictures[_InImage.m_name] = _InImage
-
-         _Width, _Height = _Image.size
-
-         _TempWidth = 0
-
-         _TempHeight = 0
-
-         _Offset = 30
-
-         _SecondTime = False
-
-         _SecondTimeWidth = False
-
-         while True:
-
-            _Left = _TempWidth
-
-            _Top = _TempHeight
-
-            _NewSize = [_Left, _Top, _Left + _Offset, _Top + _Offset]
-
-            if (_NewSize[2] > _Width): _NewSize[2] = _Width
-
-            if (_NewSize[3] > _Height): _NewSize[3] = _Height
-
-            _NewPicture = _Image.crop(_NewSize)
-
-            _Hash = hashlib.md5(_NewPicture.tostring()).hexdigest()
-
-            try:
-            
-               _Hashes[_Hash]
-
-               _Hashes[_Hash].append(_InImage)
-
-            except:
- 
-               _Hashes[_Hash] = list().append(_InImage)
-
-            _InImage.m_pictures.append(_Hash)
-
-            _TempWidth = _TempWidth + _Offset
-
-            if (_TempWidth > _Width):
-
-               _TempWidth = 0
-
-               _TempHeight = _TempHeight + _Offset
-
-            if (_TempHeight >= _Height):
-
-               break
+      build_dict(_Directory)
